3b13e5c19dfe5456c35f14edc29888f8459cc845
mustafa-bircan <msbrtfcn@gmail.com>
2025-03-12 12:26:05 +0300

    Create & GasPoweredCar.java

---
+++ src/main/java/org/example/arge/GasPoweredCar.java
@@ -0,0 +1,18 @@
+package org.example.arge;
+
+public class GasPoweredCar extends CarSkeleton {
+    private double avgKmPerLitre;
+    private int cylinders;
+
+
+    public GasPoweredCar(String name, String description, double avgKmPerLitre, int cylinders) {
+        super(name, description);
+        this.avgKmPerLitre = avgKmPerLitre;
+        this.cylinders = cylinders;
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.println("Gas-powered car engine is starting with " + cylinders + " cylinders.");
+    }
+}
